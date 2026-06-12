01e4ee86653326e61a4c25593a3da7e14383a629
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-06-12 09:46:25 +0330

    use better-auth/minimal instead of full better-auth package

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { db } from '@/db'
-import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { betterAuth } from 'better-auth/minimal'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
 import { serverEnv } from './env.server'
 
