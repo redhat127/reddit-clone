c14dc6984b4dff1f78de77c09024d530f227ff4b
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-06-13 08:18:41 +0330

    feat: add 404 not-found component

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,12 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import {
+  createRouter as createTanStackRouter,
+  Link,
+} from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
+import { ChevronLeftIcon } from 'lucide-react'
+import { Button } from './components/ui/button'
 import { getContext } from './integrations/tanstack-query/root-provider'
 
 export function getRouter() {
@@ -15,7 +20,20 @@ export function getRouter() {
     defaultPreloadStaleTime: 0,
 
     defaultNotFoundComponent() {
-      return null
+      return (
+        <div className="max-w-sm mx-auto text-center mt-16">
+          <h1 className="text-red-600 dark:text-red-400 font-bold text-3xl mb-2">
+            404 - یافت نشد
+          </h1>
+          <p className="mb-4">صفحه ی مورد نظر یافت نشد.</p>
+          <Button type="button" asChild className="px-8">
+            <Link to="/">
+              <ChevronLeftIcon />
+              برگرد به خانه
+            </Link>
+          </Button>
+        </div>
+      )
     },
   })
 
